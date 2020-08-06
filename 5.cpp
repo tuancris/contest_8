44ba433360612611f2c763e42a4313b95730d59e
tuancris <62863015+tuancris@users.noreply.github.com>
2020-08-06 21:06:39 +0700

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,32 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string tobinary(int n) {
-    string res = "";
-    while(n>0) {
-        res = string(1,n%2 + '0') + res;
-        n /= 2;
-    }
-    return res;
-}
-void solve(int n) {
-    string aa = tobinary(n);
-    queue<string> q;
-    q.push("1");
-    string t = q.front(); q.pop();
-    while(t!=aa) {
-        cout << t << " ";
-        q.push(t+"0");
-        q.push(t+"1");
-        t = q.front(); q.pop();
-    }
-    cout << t << endl;
+void sinh(int n) {
+	queue<string> q;
+	q.push("1");
+	while(n--) {
+		string s1 = q.front(); q.pop();
+		cout << s1 << " ";
+		string s2 = s1;
+		q.push(s1.append("0"));
+		q.push(s2.append("1"));
+	}
 }
 int main() {
-    int t; cin >> t;
-    while(t--) {
-        int n; cin >> n;
-        solve(n);
-    }
-    return 0;
+	int t; cin >> t;
+	while(t--) {
+		int n; cin >> n;
+	    sinh(n);
+	    cout << endl;
+	}
+	return 0;
 }
